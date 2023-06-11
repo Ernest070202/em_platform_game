aa93aa3ac9ad10e88d8954084fc97ca7466e6ee2
Ernest070202 <120501113+Ernest070202@users.noreply.github.com>
2023-06-11 18:26:29 +0200

    Add files via upload

--- CameraController.cs
+++ CameraController.cs
@@ -6,9 +6,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]private Transform player;
+    Vector2 BoxSize;
+    Vector2 BoxPos;
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask CameraStop;
+
+    private void Start()
+    {
+        BoxSize = new Vector2(10, 10);
+    }
+
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y+1, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y + 1, transform.position.z);
+        BoxPos = new Vector2(player.position.x, player.position.y);
+        IsChasm();
+    }
+
+    private bool IsChasm()
+    {
+
+        if (Physics2D.BoxCast(BoxPos, BoxSize , 0f, Vector2.down, .1f, CameraStop))
+        {
+            Debug.Log("IsChasm Collision");
+            return true;
+        }
+        else return false;
     }
 }
+
+
